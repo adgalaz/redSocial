db3f7931e966af28142bc54a61fd24e199140701
adgalaz <adgalaz@gmail.com>
2018-01-05 01:41:11 -0300

    ultima actualizacion de archivos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,40 @@
-$(document).ready(function(){
-    $('.slider').slider();
+$(document).ready(function() {
+  $('.slider').slider();
 
   Materialize.updateTextFields();
 
   $('input#input_text, textarea#textarea1').characterCounter();
 
- $(".button-collapse").sideNav();
+  $(".button-collapse").sideNav();
 
- $('#btn').click(function(){
-   var comentario = $('#comment').val();
-   $('#comment').val("");
-   var contenedor = $('#cont');
+  $('.modal').modal();
 
-   contenedor.append('<div style="color:#000;"><ul class="collection"><li class="collection-item avatar"><p class="txt">' + comentario + '</p><a href="#!" class="secondary-content"><i class="material-icons basura">delete_forever</i><i class="material-icons corazon">favorite</i></a></li></ul></div>');
-   deleteContact();
-   favotite();
- });
+  $('.parallax').parallax();
+
+  $('.carousel').carousel();
+
+  $('.collapsible').collapsible();
+
+  $('#btn').click(function() {
+    var comentario = $('#comment').val();
+    $('#comment').val("");
+    var contenedor = $('#cont');
+
+    contenedor.append('<div style="color:#000;"><ul class="collection"><li class="collection-item avatar"><p class="txt">' + comentario + '</p><a href="#!" class="secondary-content"><i class="material-icons basura">delete_forever</i><i class="material-icons heart">favorite</i></a></li></ul></div>');
+    deleteContact();
+    favorite();
+  });
 
 });
 
-function deleteContact(){
-  $('.basura').click(function(){
+function deleteContact() {
+  $('.basura').click(function() {
     $(this).parent().parent().parent().remove();
   })
 }
 
-function favorite(){
-  $('.corazon').click(function(){
-    $(this).parent().parent().parent().addClass(corazon);
+function favorite() {
+  $('.heart').click(function() {
+    $(this).parent().parent().parent().addClass("corazon");
   })
 }
